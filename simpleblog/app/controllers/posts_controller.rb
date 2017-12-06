a2a86b90c7ebe2fc356eea85bfc3ae59b34c4f01
KarinGarciaZ <oscarin962011@hotmail.com>
2017-12-06 13:00:57 -0600

    Added the update and delete method to the posts.

--- simpleblog/app/controllers/posts_controller.rb
+++ simpleblog/app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
 
   def new
-
+    @post = Post.new
   end
 
   def show
@@ -15,11 +15,37 @@ class PostsController < ApplicationController
     #render plain: params[:post].inspect
     @post = Post.new(post_params)
 
-    @post.save
-    redirect_to @post
+    if (@post.save)
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
-  private def post_params
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if (@post.update(post_params))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+
+  private 
+  
+  def post_params
     params.require(:post).permit(:title, :body)
   end 
 end
