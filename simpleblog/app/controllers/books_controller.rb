50ad4f842841a634ecf65b9e74a63b83c92eab99
KarinGarciaZ <oscarin962011@hotmail.com>
2017-12-07 16:47:12 -0600

    Added a new controller named Comment for comments.

--- simpleblog/app/controllers/books_controller.rb
+++ simpleblog/app/controllers/books_controller.rb
@@ -23,6 +23,27 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    
+    if (@book.update(book_params))
+      redirect_to @book
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+
+    @book.destroy
+    redirect_to books_path
+  end
+
   private 
   
   def book_params
--- simpleblog/db/schema.rb
+++ simpleblog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171207022430) do
+ActiveRecord::Schema.define(version: 20171207210224) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20171207022430) do
     t.string   "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "username"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
